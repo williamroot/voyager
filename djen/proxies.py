37e41ab27f3f0947735977ef636e74b4014f4d6f
William Souza <williamalvesroot@gmail.com>
2026-05-04 20:56:33 -0300

    revert: country=BR no endpoint fallback ProxyScrape

--- djen/proxies.py
+++ djen/proxies.py
@@ -136,7 +136,7 @@ class ProxyScrapePool:
         'https://api.proxyscrape.com/v2/account/datacenter_shared/proxy-list'
         '?auth={key}&type=getproxies&protocol=http&format=normal&country=BR',
         'https://api.proxyscrape.com/v2/?request=getproxies'
-        '&auth={key}&protocol=http&country=all',
+        '&auth={key}&protocol=http&country=BR',
     ]
 
     def refresh(self) -> int:
